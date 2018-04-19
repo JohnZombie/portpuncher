6deeb108974d20de8c604f1968198386be272c60
J0HN Z0MB1E <johnzombie1992@gmail.com>
2018-04-18 22:49:32 -0400

    Added a class and functions

--- scanarg.py
+++ scanarg.py
@@ -5,24 +5,29 @@ import sys
 
 os.system('clear')
 
-if len(sys.argv) != 3:
-    print("Please type in an ip address then port. Ex: python3 socks.py 192.168.1.1 80")
-    sys.exit(1)
-ip = sys.argv[1]
-port = sys.argv[2]
-socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-data = bytes.fromhex('520500050106EF')
+class Scanner(object):
+    def args(self):
+        if len(sys.argv) != 3:
+            print("Please type in an ip address then port. Ex: python3 socks.py 192.168.1.1 80")
+            sys.exit(1)
+        ip = sys.argv[1]
+        port = sys.argv[2]
+        return ip, port
+    def scanport(self, targethost, targetport):
+        socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        data = bytes.fromhex('520500050106EF')
 
-try:
-    socks.connect((ip,int(port)))
-    socks.settimeout(5)
-    socks.sendall(data)
-    print("[*] Port", port, "Is open!")
+        try:
+            socks.connect((ip,int(port)))
+            socks.settimeout(5)
+            socks.sendall(data)
+            print("[*] Port", port, "Is open!")
 
-except socket.error:
-    print("[*] Port", port, "Is closed....Sorry")
-    sys.exit()
+        except socket.error:
+            print(socket.error)
+            sys.exit()
 
+ip, port = Scanner().args()
 
-
+Scanner().scanport(ip, port)
 
