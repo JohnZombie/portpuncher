deb92b5e07f413ac545e661c4a3a627ff1f336b6
J0HN Z0MB1E <johnzombie1992@gmail.com>
2018-07-05 19:27:55 -0400

    implemented argparse

--- portpunch.py
+++ portpunch.py
@@ -2,24 +2,29 @@
 import socket
 import os
 import sys
+import argparse
 
 os.system('clear')
 
 class Scanner(object):
     def Args(self):
-        if len(sys.argv) != 3:
-            print("~Please type in an ip address followed by the port you wish to punch... Ex: python3 socks.py 192.168.1.1 80")
-            sys.exit(1)
-        ip = sys.argv[1]
-        port = sys.argv[2]
-        return ip, port
-    
-    def ScanPort(self, targethost, targetport):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-p","--port",help="[*]Use this option followed by the port you wish to punch", action='store',type=int)
+        parser.add_argument("-t","--target",help="[*]Use this option followed by the port you wish to punch", action='store')
+        args = parser.parse_args()
+        port = args.port
+        target = args.target
+        if (port == None) | (target == None):
+            print(parser.usage)
+            exit(0)
+        return port,target
+
+    def ScanPort(self, port, target):
         socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         data = bytes.fromhex('520500050106EF')
 
         try:
-            socks.connect((ip,int(port)))
+            socks.connect((target,int(port)))
             socks.settimeout(5)
             socks.sendall(data)
             print("[*] Port", port, "Is open!")
@@ -27,8 +32,11 @@ class Scanner(object):
         except socket.error:
             print("[*]Port",port, "Is closed :(")
             sys.exit()
+    
+    
+def main():
+    target,port = Scanner().Args()
+    Scanner().ScanPort(target,port)
 
-ip, port = Scanner().Args()
-
-Scanner().ScanPort(ip, port)
+main()
 
