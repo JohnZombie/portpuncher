89f59de8e8eae1bba4e09eecb0002245ea30f47c
John Zombie <johnzombie1992@gmail.com>
2018-07-05 12:56:36 -0400

    MAde changes

--- portpunch.py
+++ portpunch.py
@@ -6,14 +6,15 @@ import sys
 os.system('clear')
 
 class Scanner(object):
-    def args(self):
+    def Args(self):
         if len(sys.argv) != 3:
-            print("Please type in an ip address then port. Ex: python3 socks.py 192.168.1.1 80")
+            print("~Please type in an ip address followed by the port you wish to punch... Ex: python3 socks.py 192.168.1.1 80")
             sys.exit(1)
         ip = sys.argv[1]
         port = sys.argv[2]
         return ip, port
-    def scanport(self, targethost, targetport):
+    
+    def ScanPort(self, targethost, targetport):
         socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         data = bytes.fromhex('520500050106EF')
 
@@ -24,10 +25,10 @@ class Scanner(object):
             print("[*] Port", port, "Is open!")
 
         except socket.error:
-            print(socket.error)
+            print("[*]Port",port, "Is closed :(")
             sys.exit()
 
-ip, port = Scanner().args()
+ip, port = Scanner().Args()
 
-Scanner().scanport(ip, port)
+Scanner().ScanPort(ip, port)
 
